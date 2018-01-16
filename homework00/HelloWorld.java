adeaff83e1a794626d7db9deed60df0bf60b5ffe
Kevin Peters <35283725+kpeter51@users.noreply.github.com>
2018-01-15 18:57:55 -0800

    homework00
    added homework00 to the repository

---
+++ homework00/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+    /*
+    Author: Kevin Peters
+    Date: 2018-01-15
+    Purpose: Say hello to the world
+    */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
