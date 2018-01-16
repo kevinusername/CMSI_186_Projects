e49ec05060b6eaa6309c70dc0cbcee966c31ff8d
Kevin Peters <35283725+kpeter51@users.noreply.github.com>
2018-01-15 20:42:36 -0800

    added new heading

--- homework00/HelloWorld.java
+++ homework00/HelloWorld.java
@@ -1,12 +1,20 @@
-public class HelloWorld
-{
-    /*
-    Author: Kevin Peters
-    Date: 2018-01-15
-    Purpose: Say hello to the world
-    */
-    public static void main(String[] args)
-    {
+public class HelloWorld {
+    /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    *  File name     :  HelloWorld.java
+    *  Purpose       :  Say "Hello" to the world
+    *  Author        :  Kevin Peters
+    *  Date          :  2018-01-15
+    *  Notes         :  None
+    *  Warnings      :  None
+    *  Exceptions    :  None
+    * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    *  Revision History
+    *  ---------------
+    *            Rev      Date     Modified by:  Reason for change/modification
+    *           -----  ----------  ------------  -----------------------------------------------------------
+    *  @version 1.0.0  2018-01-15  Kevin Peters  Initial writing and release
+    * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
