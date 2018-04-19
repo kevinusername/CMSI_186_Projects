2fa1d6239e69a96ba4edef5a2807182b218cbab7
Kevin Peters <35283725+kpeter51@users.noreply.github.com>
2018-04-19 00:08:10 -0700

    Added javadocs and started multiplication

--- homework06/BrobInt.java
+++ homework06/BrobInt.java
@@ -10,8 +10,15 @@ public class BrobInt {
     private String internalValue = "";
     private boolean isNegative = false;
     private byte[] byteVersion = null;
+    private byte[] digitArray = null;
     private int length = 0;
 
+    /**
+    *  Constructor takes a string and assigns it to the internal storage, checks for a sign character
+    *   and handles that accordingly;  it then checks to see if it's all valid digits, and reverses it
+    *   for later use
+    *  @param  value  String value to make into a BrobInt
+    */
     public BrobInt(String value) {
 
         if (value.charAt(0) == '-') { // Handles Negative numbers
@@ -36,6 +43,28 @@ public class BrobInt {
         }
     }
 
+    /**
+     * Creates an byte array that holds one digit in each location
+     * Planned use for multiplication and divison
+     */
+    public void createDigitArray() {
+        digitArray = new byte[length];
+
+        for (int i = 0; i < length; i++) {
+            try {
+                digitArray[i] = Byte.parseByte(internalValue.substring(i, i + 1));
+            } catch (NumberFormatException nfe) {
+                System.out.println("Please enter a valid Decimal only input");
+                System.exit(1);
+            }
+        }
+    }
+
+    /**
+    *  Method to add the value of a BrobIntk passed as argument to this BrobInt using byte array
+    *  @param  gint         BrobInt to add to this
+    *  @return BrobInt that is the sum of the value of this BrobInt and the one passed in
+    *   */
     public BrobInt add(BrobInt gint) {
 
         boolean addSign = false;
@@ -88,6 +117,11 @@ public class BrobInt {
         return new BrobInt(answer);
     }
 
+    /** 
+    *  Method to subtract the value of a BrobIntk passed as argument to this BrobInt using bytes
+    *  @param  gint         BrobInt to subtract from this
+    *  @return BrobInt that is the difference of the value of this BrobInt and the one passed in
+    *   */
     public BrobInt subtract(BrobInt gint) {
 
         if (equals(gint)) {
@@ -138,18 +172,64 @@ public class BrobInt {
 
     }
 
+    /** 
+    *  Method to multiply the value of a BrobIntk passed as argument to this BrobInt
+    *  @param  gint         BrobInt to multiply by this
+    *  @return BrobInt that is the product of the value of this BrobInt and the one passed in
+    *   */
     public BrobInt multiply(BrobInt gint) {
+
+        // BrobInt[] tempBrobArray = setBigger(gint);
+        // BrobInt bigBrob = new BrobInt(tempBrobArray[0].toString());
+        // BrobInt littleBrob = new BrobInt(tempBrobArray[1].toString());
+
+        // bigBrob.createDigitArray();
+        // littleBrob.createDigitArray();
+
+        // byte[] productArray = new byte[bigBrob.length + littleBrob.length];
+
+        // for (int i = 0; i < littleBrob.length; i++) {
+        //     for (int j = 0; j < bigBrob.length; j++) {
+        //         productArray[j] = (byte) (bigBrob.digitArray[j] * littleBrob.digitArray[i]);
+        //         try {
+        //             if (productArray[j] > 9) {
+        //                 bigBrob.digitArray[j + 1] += bigBrob.digitArray[j] / littleBrob.digitArray[i];
+        //                 productArray[j] = (byte) (productArray[j] % 10);
+        //             }
+        //         } catch (ArrayIndexOutOfBoundsException aob) {
+        //             productArray[j] += bigBrob.digitArray[j] / littleBrob.digitArray[i];
+        //             productArray[j + 1] = (byte) (productArray[j] % 10);
+        //         }
+        //     }
+        // }
+        // System.out.println(Arrays.toString(productArray));
+        // return ZERO;
         throw new UnsupportedOperationException("\n         Sorry, that operation is not yet implemented.");
+
     }
 
+    /** 
+    *  Method to divide the value of this BrobIntk by the BrobInt passed as argument
+    *  @param  gint         BrobInt to divide this by
+    *  @return BrobInt that is the dividend of this BrobInt divided by the one passed in
+    *   */
     public BrobInt divide(BrobInt gint) {
         throw new UnsupportedOperationException("\n         Sorry, that operation is not yet implemented.");
     }
 
+    /** 
+    *  Method to get the remainder of division of this BrobInt by the one passed as argument
+    *  @param  gint         BrobInt to divide this one by
+    *  @return BrobInt that is the remainder of division of this BrobInt by the one passed in
+    *   */
     public BrobInt remainder(BrobInt gint) {
         throw new UnsupportedOperationException("\n         Sorry, that operation is not yet implemented.");
     }
 
+    /** 
+    *  Method to return a String representation of this BrobInt
+    *  @return String  which is the String representation of this BrobInt
+    *   */
     public String toString() {
         if (isNegative) {
             return "-".concat(internalValue);
@@ -158,6 +238,14 @@ public class BrobInt {
         }
     }
 
+    /** 
+    *  Method to compare a BrobInt passed as argument to this BrobInt
+    *  @param  gint  BrobInt to add to this
+    *  @return int   that is one of neg/0/pos if this BrobInt precedes/equals/follows the argument
+    *  NOTE: this method does not do a lexicographical comparison using the java String "compareTo()" method
+    *        It takes into account the length of the two numbers, and if that isn't enough it does a
+    *        character by character comparison to determine
+    *   */
     public int compareTo(BrobInt gint) {
 
         if (false == isNegative && gint.isNegative) {
@@ -176,6 +264,12 @@ public class BrobInt {
 
     }
 
+    /**
+     * Creates a BrobInt array with location 0 being the bigger of the two and location 1 being the smaller
+     * Used for keeping arthimatic operations simpler
+     * @param gint BrobInt to compare to
+     * @return BrobInt[] with order in respect to size
+     */
     public BrobInt[] setBigger(BrobInt gint) {
         BrobInt[] tempBrobArray = new BrobInt[2];
         if (compareTo(gint) == 0 || compareTo(gint) > 0) {
@@ -188,6 +282,11 @@ public class BrobInt {
         return tempBrobArray;
     }
 
+    /**
+     * takes an array of bytes (where each location has at most 2 digits)
+     *  creates a string by placing each byte in reverse order
+     * @return String representation of byteArray
+     */
     public static String byteToString(byte[] bArray) {
         StringBuilder answer = new StringBuilder();
         for (int i = bArray.length - 1; i >= 0; i--) {
@@ -200,12 +299,24 @@ public class BrobInt {
         return answer.toString();
     }
 
+    /** 
+    *  Method to check if a BrobInt passed as argument is equal to this BrobInt
+    *  @param  gint     BrobInt to compare to this
+    *  @return boolean  that is true if they are equal and false otherwise
+    *  NOTE: this method performs a similar lexicographical comparison as the "compareTo()" method above
+    *        also using the java String "equals()" method -- THAT was easy, too..........
+    *   */
     public boolean equals(Object x) {
         return this == x;
     }
 
+    /** 
+    *  Method to return a BrobInt given a long value passed as argument
+    *  @param  value         long type number to make into a BrobInt
+    *  @return BrobInt  which is the BrobInt representation of the long
+    *   */
     public static BrobInt valueOf(long value) {
-        throw new UnsupportedOperationException("\n         Sorry, that operation is not yet implemented.");
+        return new BrobInt(Long.toString(value));
     }
 
     public static String trimZeros(String s) {
@@ -216,11 +327,14 @@ public class BrobInt {
         return answer.toString();
     }
 
+    /** 
+    *  the main method redirects the user to the test class
+    *  @param  args  String array which contains command line arguments
+    *  note:  we don't really care about these
+    *   */
     public static void main(String[] args) {
-        BrobInt myBrob = new BrobInt("-238572387509237523");
-        BrobInt myBrob2 = new BrobInt("-74563457457345734573457345");
-
-        System.out.println(new BrobInt("12586269025").add(new BrobInt("20365011074")).internalValue);
+        BrobInt myBrob = new BrobInt("55");
+        BrobInt myBrob2 = new BrobInt("2");
 
     }
 }
